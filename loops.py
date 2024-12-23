b64c22a25ff4a4100e4c2a6cf4fc2b29edeaa21c
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-12-23 15:29:43 +0800

    while loop

---
+++ loops.py
@@ -0,0 +1,20 @@
+#while loop
+
+# i = 1; 
+# while i <= 6:
+#     print(i)
+#     i+=1
+
+
+#break in while
+
+i = 1
+while i <6:
+    i+=1
+    if(i == 3):
+        continue
+    print(i)
+else: 
+    print('finsiehs ')
+    
+
